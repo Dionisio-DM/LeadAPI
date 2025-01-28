0ed1ab05cc4b463a76a8f9ea26128507ef74e132
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-28 16:34:07 -0300

    add rota de teste /status

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(cors());
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
