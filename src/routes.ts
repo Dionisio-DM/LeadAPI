be6701142a829bfe647f396bf2043f5d2b5c72eb
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-28 18:49:04 -0300

    feat: criação controlador para leads e adição do mesmo nas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { LeadsController } from "./controllers/LeadsControllers";
 
 const router = Router();
+
+const leadsController = new LeadsController();
+
+router.get("/leads", leadsController.index);
 
 router.get("/status", (req, res) => {
   res.json({ message: "OK" });
