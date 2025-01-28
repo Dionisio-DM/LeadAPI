834414d4f96de1e89174cab08ce848c0035d7f43
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-28 20:02:50 -0300

    feat: schema de validação com zod de leads criado

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express from "express";
 import { router } from "./routes";
+import { json } from "stream/consumers";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
