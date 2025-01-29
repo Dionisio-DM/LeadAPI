33059caef3d30554518a1f6a48bc8ee7773c1b91
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-28 22:23:47 -0300

    feat: novo schema para busca avançada

--- src/controllers/Schemas/LeadsRequestSchemas.ts
+++ src/controllers/Schemas/LeadsRequestSchemas.ts
@@ -1,20 +1,29 @@
 import { z } from "zod";
 
+const LeadStatusSchema = z.enum([
+  "New",
+  "Contacted",
+  "Qualified",
+  "Converted",
+  "Unresponsive",
+  "Disqualified",
+  "Archived",
+]);
+
+const GetLeadsRequestSchema = z.object({
+  page: z.string().optional(),
+  pageSize: z.string().optional(),
+  name: z.string().optional(),
+  status: LeadStatusSchema.optional(),
+  sortBy: z.enum(["name", "status"]).optional(),
+  order: z.enum(["desc", "asc"]).optional(),
+});
+
 const CreateLeadRequestSchema = z.object({
   name: z.string(),
   email: z.string(),
   phone: z.string(),
-  status: z
-    .enum([
-      "New",
-      "Contacted",
-      "Qualified",
-      "Converted",
-      "Unresponsive",
-      "Disqualified",
-      "Archived",
-    ])
-    .optional(),
+  status: LeadStatusSchema.optional(),
 });
 
 const UpdateLeadRequestSchema = z
@@ -22,21 +31,15 @@ const UpdateLeadRequestSchema = z
     name: z.string().optional(),
     email: z.string().optional(),
     phone: z.string().optional(),
-    status: z
-      .enum([
-        "New",
-        "Contacted",
-        "Qualified",
-        "Converted",
-        "Unresponsive",
-        "Disqualified",
-        "Archived",
-      ])
-      .optional(),
+    status: LeadStatusSchema.optional(),
   })
   .transform((data) => ({
     ...data,
     updatedAt: new Date(),
   }));
 
-export { CreateLeadRequestSchema, UpdateLeadRequestSchema };
+export {
+  CreateLeadRequestSchema,
+  UpdateLeadRequestSchema,
+  GetLeadsRequestSchema,
+};
