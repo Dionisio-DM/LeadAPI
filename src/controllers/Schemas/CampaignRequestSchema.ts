24278ee0c7a485173858733f8d52de19045a98f6
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-29 15:25:58 -0300

    feat: add schema validação tabela campaign

---
+++ src/controllers/Schemas/CampaignRequestSchema.ts
@@ -0,0 +1,22 @@
+import { start } from "repl";
+import { z } from "zod";
+
+const GetCampaignRequestSchema = z.object({
+  page: z.string().optional(),
+  pageSize: z.string().optional(),
+  name: z.string().optional(),
+  description: z.string().optional(),
+  sortBy: z.enum(["name", "createdAt", "updatedAt"]).optional(),
+  order: z.enum(["desc", "asc"]),
+});
+
+const CreateCampaignRequestSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  startDate: z.date().optional(),
+});
+
+const UpdateCampaignRequestSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+});
