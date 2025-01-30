5377a2f5e6bf7ff558dba50b6e4bc28bda86dec2
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-01-30 12:34:57 -0300

    feat: implementando interface groups

--- src/container.ts
+++ src/container.ts
@@ -4,10 +4,13 @@ import { CampaignsController } from "./controllers/CampaignController";
 import { CampaignLeadsController } from "./controllers/CampaignLeadsController";
 import { GroupLeadsController } from "./controllers/GroupLeadsController";
 import { PrismaLeadsRepository } from "./repositories/prisma/PrismaLeadsRepository";
+import { PrismaGroupsRepository } from "./repositories/prisma/PrismaGroupsRepository";
 
 const leadsRepository = new PrismaLeadsRepository();
+const groupsRepository = new PrismaGroupsRepository();
+
 const leadsController = new LeadsController(leadsRepository);
-const groupsController = new GroupsController();
+const groupsController = new GroupsController(groupsRepository);
 const campaignsController = new CampaignsController();
 const campaignLeadsController = new CampaignLeadsController();
 const groupLeadsController = new GroupLeadsController();
